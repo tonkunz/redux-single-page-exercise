539433a26342a86a9cb4c112e3f5924cccdc5093
Everton Kunz <ehokeverton@gmail.com>
2019-01-21 20:01:33 -0200

    Iniciando: Recuperando o estado

---
+++ index.js
@@ -0,0 +1,15 @@
+function createStore () {
+  // Store deve possuir 4 partes, sendo:
+  // 1. O estado da aplicação (the State).
+  // 2. Um método para obter o estado atual.
+  // 3. Funções de monitoramento (listener functions) que serão chamadas pelo armazenador quando o estado for modificado.
+  // 4. Método para alterações no estado do armazenador. (Update State)
+
+  let state
+
+  const getState = () => state
+
+  return {
+    getState
+  }
+} 
